90e206f6731b0c725bd23275b2ffae0b2d963ec5
primovere <p.8izo3@slmail.me>
2022-07-06 10:08:34 +0800

    Add a button and put some code into a function

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-
-for (let i = 0; i < 16; i++) {
-  const flexContainer = document.createElement("div");
-  flexContainer.classList.add("flex-container");
-  container.appendChild(flexContainer);
-  flexContainer.style.display = "flex";
-  for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    flexContainer.appendChild(div);
-    div.setAttribute("style", "height: 20px; width: 20px; border: 1px solid black;");
-    div.addEventListener("mouseenter", () => div.classList.add("hover"));
+function createGrid(num=16) {
+  for (let i = 0; i < num; i++) {
+    const flexContainer = document.createElement("div");
+    flexContainer.classList.add("flex-container");
+    container.appendChild(flexContainer);
+    flexContainer.style.display = "flex";
+    for (let i = 0; i < num; i++) {
+      const div = document.createElement("div");
+      flexContainer.appendChild(div);
+      div.setAttribute("style", "height: 20px; width: 20px; border: 1px solid black;");
+      div.addEventListener("mouseenter", () => div.classList.add("hover"));
+    }
   }
 }
+
+button.addEventListener("click", () => {
+  let num = parseInt(prompt("How many squares would you like per side for the new grid? (Enter the number from 1 to 100)"))
+  if (0 < num <= 100) {
+    container.remove;
+    createGrid(num);
+  }
+});
+
+createGrid();
